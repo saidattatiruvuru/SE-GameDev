98d479dacb87377be52f359766fface14000c7ef
SwethaRajakumar2001 <cs18b037@iittp.ac.in>
2021-01-30 23:25:10 +0530

    Coin render and collection

---
+++ Discovery/Assets/scripts/CoinPicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicker : MonoBehaviour
+{
+
+    private float coin = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.tag == "Coin")
+        {
+            coin++;
+            Destroy(other.gameObject);
+        }
+    }
+}
