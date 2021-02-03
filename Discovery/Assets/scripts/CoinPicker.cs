8ece142527573ae10be7aaf93e01354f6a5cebb7
SwethaRajakumar2001 <cs18b037@iittp.ac.in>
2021-02-03 23:21:28 +0530

    Level Success stuff handling(The UI, orbs handling and go to collection))

--- Discovery/Assets/scripts/CoinPicker.cs
+++ Discovery/Assets/scripts/CoinPicker.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class CoinPicker : MonoBehaviour
 {
+    public int LevelOrbs = 1;
+    private int coin = 0;
+    public int Level = 1;
 
-    private float coin = 0;
+    private float orb = 0;
 
     public TextMeshProUGUI score;
+    public TextMeshProUGUI Finalscore;
+
+    public Canvas SuccessCanvas;
+
+    public void GoToCollection()
+    {
+        int finScr = 0;
+        if(PlayerPrefs.HasKey("Score"))
+        {
+            finScr = PlayerPrefs.GetInt("Score");
+        }
+        finScr += coin;
+        PlayerPrefs.SetInt("Score", finScr);
+
+        if (PlayerPrefs.HasKey("MaxLevel")&& PlayerPrefs.GetInt("MaxLevel")<Level || !PlayerPrefs.HasKey("MaxLevel"))
+        {
+            PlayerPrefs.SetInt("MaxLevel", Level);
+        }
+
+        PlayerPrefs.SetString("HomePage", "Collection");
+        SceneManager.LoadScene(0);
+    }
 
     void OnStart()
     {
         score = GetComponent<TextMeshProUGUI>();
+        SuccessCanvas = GetComponent<Canvas>();
+        SuccessCanvas.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -24,6 +52,26 @@ public class CoinPicker : MonoBehaviour
             coin++;
             Debug.Log(coin);
             score.text = coin.ToString();
+        }
+
+        else if (other.gameObject.layer == 11)
+        {
+            other.transform.tag = "Collected";
+            other.gameObject.SetActive(false);
+            orb++;
+            Debug.Log("Orb");
+            Debug.Log(orb);
+        }
+
+        else if (other.transform.tag == "LevelFinish")
+        {
+            if(orb == LevelOrbs)
+            {
+                SuccessCanvas.gameObject.SetActive(true);
+                Finalscore.text = coin.ToString();
+            }
+            Debug.Log("FINISHED");
+            Debug.Log(orb);
         }
     }
 }
